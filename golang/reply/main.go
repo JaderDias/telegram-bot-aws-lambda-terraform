7617827f495741787376f6a9e1bd96985af7b3bd
Jader Dias <52470+JaderDias@users.noreply.github.com>
2022-07-01 09:41:32 +0200

    testing noop performance

--- golang/reply/main.go
+++ golang/reply/main.go
@@ -3,12 +3,7 @@ package main
 import (
 	"context"
 	"errors"
-	"fmt"
-	"log"
-	"os"
-	"strings"
 
-	telegram "example.com/telegram"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
@@ -18,14 +13,6 @@ var (
 )
 
 func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	log.Printf("Body size = %d. \n", len(request.Body))
-	logRequest := fmt.Sprintf("%+v", request)
-	log.Printf("Request: %s", strings.ReplaceAll(logRequest, "\n", `\n`))
-
-	s3BucketId := os.Getenv("s3_bucket_id")
-	languageCode := os.Getenv("language")
-	tokenParameterName := os.Getenv("token_parameter_name")
-	telegram.Reply(ctx, request.Body, s3BucketId, languageCode, tokenParameterName)
 	return events.APIGatewayProxyResponse{Body: "POST", StatusCode: 200}, nil
 }
 
